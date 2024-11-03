1b882d3d313e1c1a55f4e78c8ee27c2a16e240ac
Dan <danpadd15@gmail.com>
2024-11-03 15:29:32 +0000

    Update MarketDataHandler.py

--- MarketDataHandler.py
+++ MarketDataHandler.py
@@ -1,9 +1,37 @@
 import requests
+import io
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as mpf
 
-url = "https://data.alpaca.markets/v2/stocks/bars"
+class marketDataHandler:
+    def __init__(self):
+        pass
 
-headers = {"accept": "application/json"}
+    def fetch_market_data(self, symbol, time_frame, limit, feed, currency):
 
-response = requests.get(url, headers=headers)
+        url = "https://data.alpaca.markets/v2/stocks/bars"
 
-print(response.text)
+        params = {
+            "symbols": symbol,          # The stock symbol to fetch data for
+            "timeframe": time_frame,       # Timeframe for the data (e.g., 1Min, 5Min, 1Day)
+            "limit": limit,             # The number of data points to fetch
+            "adjustment": 'raw',       # Type of adjustment (e.g., raw, split, dividend)
+            "feed": feed,             # The data feed source
+            "currency": currency,         # The currency of the data
+            "sort": "asc"              # Sort order (e.g., ascending or descending)
+        }
+
+        headers = {
+            "Authorization": "Bearer YOUR_API_KEY"
+        }
+
+        response = requests.get(url, headers=headers, params=params)
+
+        data = response.json()
+
+    def market_open_check()-> bool:
+        #Checks time and to see if market is open/closed, if open request current data, if closed retrieve data from DB
+        return
+    
