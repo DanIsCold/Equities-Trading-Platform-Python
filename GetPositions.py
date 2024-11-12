57ec604653c91398c92f634f1194cb5a063e2c8f
Elliott <45100766+elliottSurenkok@users.noreply.github.com>
2024-11-12 20:36:10 +0000

    Update GetPositions.py

--- GetPositions.py
+++ GetPositions.py
@@ -1,4 +1,5 @@
 from alpaca.trading.client import TradingClient
+from alpaca.trading.requests import GetAssetsRequest
 
 trading_client = TradingClient('PK14CV687K30ALR8RMUG', 'Fh19ACB7Kb98LwPB3Wsiu7koQCaaEA08Rt5PiXsP', paper=True)
 
@@ -6,7 +7,7 @@ class getPositions():
     def __init__(self) -> None:
         pass
 
-    def get_stock_pos(self,symbol: str):
+    def get_stock_position(self,symbol: str):
         return trading_client.get_open_position(symbol)
     
     def get_all_positions(self):
@@ -15,4 +16,11 @@ class getPositions():
         for position in portfolio:
             print(f"{position.qty} shares of {position.symbol}")
 
+    def portfolio_gain_loss(self):
+        account = trading_client.get_account()
+        balance_change = float(account.equity) - float(account.last_equity)
+        print(f'Today\'s portfolio balance change: ${balance_change}')
+
 joe = getPositions().get_all_positions()
+
+
