81f3883fafe07f7687044dba418028b4b080b10e
Dan <danpadd15@gmail.com>
2024-11-29 17:35:37 +0000

    Create LiveDataHandler.py

---
+++ PROD/LiveDataHandler.py
@@ -0,0 +1,30 @@
+class LiveDataHandler:
+    def __init__(self, db_config, api_key, ):
+
+        def __init__(self, websocket_url, api_key, secret_key, db_config):
+            #initialize the websocket url
+            pass
+
+        def authenticate(self, websocket):
+            #authenticate the websocket connection
+            pass
+            
+        def subscribe(self, websocket, symbols):
+            #subscribe to the given symbols
+            pass
+
+        def process_message(self, message):
+            #process the incoming message
+            pass
+
+        def store_message(self, message):
+            #store the message in the database
+            pass
+
+        def connect(self, symbols):
+            #connect to the websocket
+            pass
+
+        def start(self, symbols):
+            #start the websocket connection
+            pass
